71f5555e872915a2fb816e954aef30c2211412aa
Sathish965 <sathishjvs12@gmail.com>
2021-03-17 15:06:55 +0530

    changed port number as it also need to support heroku system

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({
 }));
 app.use(express.static("public"));
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("App listening on port 3000");
 });
 
